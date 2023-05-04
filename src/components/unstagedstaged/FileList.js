d9ee221b427e3770efbf217daa9b4fcf4d504a8a
allen <wlq7410@naver.com>
2023-05-04 14:45:46 +0900

    feat: file list를 인자로 받아서 리스트를 출력하고 선택 가능하도록 변경

--- src/components/unstagedstaged/FileList.js
+++ src/components/unstagedstaged/FileList.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FileList = () => {
-  return (
-    <>
-      <div>file list</div>;
-    </>
-  );
+const FileList = ({ files }) => {
+    const [selectedFiles, setSelectedFiles] = useState([]);
+
+    const handleFileClick = (file) => {
+        if (selectedFiles.includes(file)) {
+            setSelectedFiles(selectedFiles.filter((selected) => selected !== file));
+        } else {
+            setSelectedFiles([...selectedFiles, file]);
+        }
+    };
+
+    return (
+        <div>
+            <ul>
+                {files.map((file, index) => (
+                    <li
+                        key={index}
+                        onClick={() => handleFileClick(file)}
+                        style={{
+                            cursor: 'pointer',
+                            backgroundColor: selectedFiles.includes(file) ? 'lightblue' : '',
+                        }}
+                    >
+                        {file.name}
+                    </li>
+                ))}
+            </ul>
+            {selectedFiles.length > 0 && (
+                <div>
+                    선택된 파일:
+                    {selectedFiles.map((file, index) => (
+                        <span key={index}>{file.name}, </span>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default FileList;
--- src/components/unstagedstaged/UnstagedStaged.js
+++ src/components/unstagedstaged/UnstagedStaged.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import FileList from './FileList';
 
+/**
+ * For untracked files:
+ *  Adding the new files into a staging area (untracked -> staged; git add)
+ *  For modified files
+ *  Adding the modified files into a staging area (modified -> staged; git add)
+ *  Undoing the modification (modified -> unmodified; git restore)
+ *  For staged files
+ * Unstaging changes (staged -> modified or untracked; git restore --staged)
+ *  For committed or unmodified files
+ *  Untracking files (committed -> untracked; git rm --cached)
+ *  Deleting files (committed -> staged; git rm)
+ *  Renaming files (committed -> staged; git mv)
+ */
+
+const files = [
+    { name: 'file1.txt' },
+    { name: 'file2.txt' },
+    { name: 'file3.txt' },
+];
+
+
 const UnstagedStaged = () => {
   return (
     <>
       <div>Staged 컴포넌트 입니다.</div>
-      <FileList />
+      <FileList files={files}/>
 
       <div>Unstaged 컴포넌트 입니다.</div>
-      <FileList />
+      <FileList files={files}/>
     </>
   );
 };
