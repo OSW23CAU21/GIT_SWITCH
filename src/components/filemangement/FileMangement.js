8ec680463237de00d3a9d8cc2a996f4f5e2526d0
MJ <mj@MJs-MacBook-Pro.local>
2023-05-06 21:15:27 +0900

    로컬스토리지 연결, electron사용.

--- src/components/filemangement/FileMangement.js
+++ src/components/filemangement/FileMangement.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState ,useEffect, useRef } from 'react';
 import './file-explorer/file-explorer.css';
+const { ipcRenderer } = window.require('electron');
 
 function FileMangement() {
   const id = 'filemanager'; // getting id when components change to Filemangement(id) to feature support "switching"
-  const osw23 = [ // folder array, test only
-    { name: 'src', type: 'folder', id: '/', hash: '/' },
-    { name: 'README.MD', type: 'file', id: '/', hash: '/' },
-    { name: 'package.json', type: 'file', id: '/', hash: '/' },
-  ];
-  const src = [ // folder array, test only
-    { name: 'index.css', type: 'file', id: 'src', hash: 'src' },
-    { name: 'app.js', type: 'file', id: 'src', hash: 'src' },
-    { name: 'index.js', type: 'file', id: 'src', hash: 'src' },
-  ];
+  const getDirInfo = async (dirPath, callback) => {
+    try {
+      const result = await ipcRenderer.invoke('ReadDir', dirPath);
+      callback(result);
+    } catch (err) {
+      console.error('Error reading directory info:', err);
+    }
+  };
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = './file-explorer/file-explorer.js';
@@ -22,7 +22,7 @@ function FileMangement() {
     script.onload = () => {
       const elem = document.getElementById(id);
       const options = {
-        initpath: [['', 'Projects (/)', { canmodify: false }]],
+        initpath: [['.', '/']],
         onfocus: function (e) {
           console.log('focus');
           console.log(e);
@@ -31,12 +31,24 @@ function FileMangement() {
           console.log('blur');
           console.log(e);
         },
-        onrefresh: function (folder, required) {// rerendering filebox.
+        onrefresh: function (folder, required) {
+          const PathArr = folder.GetPath().at(-1);
+          console.log(PathArr);
+          var dirPath = '';
+          for(var i = 0; i < PathArr.length - 1; i++){
+            dirPath = dirPath + PathArr[i];
+          }
+          console.log(dirPath);
+          getDirInfo(dirPath, (contents) => {
+            console.log('Directory Elements:' , contents);
+            console.log('Directory Path:',folder.GetPath());
+            folder.SetEntries(contents);
+          });
+         
         },
       };
       const fe = new window.FileExplorer(elem, options);
       const folder = fe.GetCurrentFolder();
-      folder.SetEntries(src); //getting folder array "src" for test.
     };
 
     return () => {
---
+++ src/main.js
@@ -0,0 +1,71 @@
+const { app, BrowserWindow, ipcMain } = require('electron') 
+const fs = require('fs')
+const path = require('path') 
+function createWindow () { 
+  const win = new BrowserWindow({ 
+    width: 800, 
+    height: 600, 
+    webPreferences: { 
+      nodeIntegration: true,
+      contextIsolation : false
+    } 
+  }) 
+  win.loadURL("http://localhost:3000")
+} 
+app.whenReady().then(() => { 
+  createWindow() 
+}) 
+app.on('window-all-closed', function () { 
+  if (process.platform !== 'darwin') app.quit() 
+})
+
+const readDirectoryInfo = (dirPath, callback) => {
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      callback(err, null);
+      return;
+    }
+
+    const DirContents = [];
+    const basePath = path.basename(dirPath);
+
+    files.forEach((file) => {
+      const filePath = path.join(dirPath, file);
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error('Error getting file stats:', err);
+          return;
+        }
+
+        const fileInfo = {
+          name: file,
+          type: stats.isFile() ? 'file' : stats.isDirectory() ? 'folder' : '',
+          id: dirPath + '/',
+          hash: '/'
+        };
+
+        DirContents.push(fileInfo);
+
+        if (DirContents.length === files.length) {
+          callback(null, DirContents);
+        }
+      });
+    });
+  });
+};
+
+ipcMain.handle('ReadDir', async (event, DirPath) => { // DirPath are string for exapmle "./osw23/team21/"
+  return new Promise((resolve, reject) => {
+    readDirectoryInfo(DirPath, (err, fileInfoList) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(fileInfoList);
+      }
+    });
+  });
+});
+
+
