05df9b264e9801ce3d70123fff356f96beb32acf
jamin-sub <sjm02046@naver.com>
2023-05-13 08:57:37 +0900

    commitbuttonclick update

--- src/components/menubar/Menubar.js
+++ src/components/menubar/Menubar.js
@@ -1,26 +1,26 @@
 import React from 'react';
-const {ipcRenderer} = window.require('electron');
+const { ipcRenderer } = window.require('electron');
 
 const setDirButtonClick = async () => {
   const rootPath = await ipcRenderer.invoke('getRoot');
-  console.log(rootPath); 
-}
+  console.log(rootPath);
+};
 
 const gitInitButtonClick = async () => {
   console.log('gitInit');
-}
+};
 
 const commitButtonClick = async () => {
   console.log('commit');
-}
+};
 
 const Menubar = () => {
   return (
-  <div>
-    <button onClick = {setDirButtonClick}>setDir</button>
-    <button onClick = {gitInitButtonClick}>gitInit</button>
-    <button onClick = {commitButtonClick}>commit</button>
-  </div>
+    <div>
+      <button onClick={setDirButtonClick}>setDir</button>
+      <button onClick={gitInitButtonClick}>gitInit</button>
+      <button onClick={commitButtonClick}>commit</button>
+    </div>
   );
 };
 
