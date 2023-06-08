fcb38ea060c8c41d53474cc455ab07c25e15e926
Wayz2u <kmj95v@gmail.com>
2023-06-09 02:45:01 +0900

    merge feature completes, added settings feature too

--- src/components/gitfab/MergeFab.js
+++ src/components/gitfab/MergeFab.js
@@ -1,32 +1,103 @@
-import { useState } from 'react';
-import { Fab, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, List, ListItem } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Fab, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, CircularProgress, Snackbar, Alert, Box } from '@mui/material';
 import MergeIcon from '@mui/icons-material/MergeTypeOutlined';
-import {styled} from '@mui/system';
+import { Autocomplete } from '@mui/material';
+import { styled } from '@mui/system';
+const { ipcRenderer } = window.require("electron");
 
 const StyledFab = styled(Fab)(({ theme }) => ({
-    position: 'fixed', 
-    bottom: theme.spacing(26), 
-    right: theme.spacing(2), 
+    position: 'fixed',
+    bottom: theme.spacing(26),
+    right: theme.spacing(2),
     color: '#0086FF',
-    backgroundColor: '#FFFFFF', 
+    backgroundColor: '#FFFFFF',
     '&:hover': {
-      backgroundColor: '#FFFFFF', 
+        backgroundColor: '#FFFFFF',
     },
-  }));
+}));
+
+const LabelText = styled(DialogContentText)(({ theme }) => ({
+    marginRight: theme.spacing(2),
+}));
 
 const MergeFab = () => {
-    
-    const buttonClick = () => {
-        console.log('fab pull');
-    }
- 
+    const [currentBranch, setCurrentBranch] = useState("");
+    const [branches, setBranches] = useState([]);
+    const [targetBranch, setTargetBranch] = useState("");
+    const [isMerging, setIsMerging] = useState(false);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+    const [snackbarMessage, setSnackbarMessage] = useState("");
+
+    useEffect(() => {
+        Promise.all([ipcRenderer.invoke('GF_branchname'), ipcRenderer.invoke('GF_branchlist')]).then((results) => {
+            const currentBranchName = results[0];
+            setCurrentBranch(currentBranchName);
+            const otherBranches = results[1].filter(branchName => branchName !== currentBranchName);
+            setBranches(otherBranches);
+        });
+    }, []);
+
+    const handleMerge = async () => {
+        setIsMerging(true);
+        const result = await ipcRenderer.invoke('GF_mergebranch', currentBranch, targetBranch);
+        setIsMerging(false);
+        setSnackbarMessage(result.message);
+        if(result.result){
+            setSnackbarSeverity("success");
+            setSnackbarOpen(true);
+            setDialogOpen(false);
+        }else{
+            setSnackbarSeverity("error");
+            setSnackbarOpen(true);
+        }
+    };
+
     return (
         <div>
-            <StyledFab aria-label="switch" onClick={buttonClick}>
+            <StyledFab aria-label="switch" onClick={() => setDialogOpen(true)}>
                 <MergeIcon />
             </StyledFab>
+            <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)} fullWidth={false} maxWidth='md'>
+                <DialogTitle>Merge Branches</DialogTitle>
+                <DialogContent>
+                    <Box display="flex" alignItems="center" marginBottom="1em">
+                        <LabelText>Current Branch:</LabelText>
+                        <Box fontWeight="bold" color='#0052CC'>{currentBranch}</Box>
+                    </Box>
+                    <Box display="flex" alignItems="center">
+                        <LabelText>Target Branch:</LabelText>
+                        <Autocomplete
+                            id="branchlist"
+                            options={branches}
+                            sx={{ width: 300 }}
+                            onChange={(event, newValue) => {
+                                setTargetBranch(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} label = {'Branch'}/>}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setDialogOpen(false)}>Close</Button>
+                    <Button onClick={handleMerge} disabled={isMerging}>
+                        {isMerging ? <CircularProgress /> : "Merge"}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={6000}
+                onClose={() => setSnackbarOpen(false)}
+            >
+                <Alert onClose={() => setSnackbarOpen(false)} severity={snackbarSeverity} variant="filled">
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
         </div>
     );
 }
 
 export default MergeFab;
+
---
+++ src/components/menubar/Store.js
@@ -0,0 +1,103 @@
+import { useState, useEffect } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, IconButton, InputAdornment, OutlinedInput, FormControl, InputLabel, Button } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { styled } from '@mui/system';
+
+const { ipcRenderer } = window.require('electron');
+
+
+const saveTokens = async (tokens) => {
+    const result = await ipcRenderer.invoke('SD_storetoken');
+    return result;
+}
+
+const saveAuthor = async (name, email) => {
+    const result = await ipcRenderer.invoke('SD_storeauthor', name, email);
+    return result;
+}
+
+export const SettingsDialog = ({ open, handleClose }) => {
+    const [gitToken, setGitToken] = useState('');
+    const [authorName, setAuthorName] = useState('');
+    const [authorEmail, setAuthorEmail] = useState('');
+    const [showAccessToken, setShowAccessToken] = useState(false);
+
+    useEffect(() => {
+        async function getInfos() {
+            const tokens = await ipcRenderer.invoke('SD_calltoken');
+            setGitToken(tokens);
+            const author = await ipcRenderer.invoke('SD_callauthor');
+            setAuthorName(author.name);
+            setAuthorEmail(author.email);
+        }
+
+        getInfos();
+    }, []);
+
+    const handleClickShowPassword = () => {
+        setShowAccessToken(!showAccessToken);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleSave = async () => {
+        await saveAuthor(authorName, authorEmail);
+        await saveTokens(gitToken);
+        handleClose();
+    }
+
+    return (
+        <div>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Settings</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Author Name"
+                        fullWidth
+                        value={authorName}
+                        onChange={(e) => setAuthorName(e.target.value)}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Author Email"
+                        fullWidth
+                        value={authorEmail}
+                        onChange={(e) => setAuthorEmail(e.target.value)}
+                    />
+                    <FormControl variant="outlined" fullWidth margin="dense">
+                        <InputLabel htmlFor="outlined-adornment-password">Personal Access Token</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={showAccessToken ? 'text' : 'password'}
+                            value={gitToken}
+                            onChange={(e) => setGitToken(e.target.value)}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {showAccessToken ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Personal Access Token"
+                        />
+                    </FormControl>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleSave}>Save</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+};
